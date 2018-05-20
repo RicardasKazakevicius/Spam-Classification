eeb4822a70c5862c42662e87a68031e85f2835b3
RicardasKazakevicius <ricardaskazak@gmail.com>
2018-05-20 16:56:59 +0300

    added comments

--- Src/Main.py
+++ Src/Main.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
 	
 	results_dir = '../Results/'
 
-	# data_dir = '../Spam-Assasin'
+	data_dir = '../Spam-Assasin'
 	# data_dir = '../Ling-Spam'
-	data_dir = '../PU1'
+	# data_dir = '../PU1'
 	# data_dir = '../Enron-small'
 
 	# number of k_fold cross-validation
@@ -37,14 +37,17 @@ if __name__ == '__main__':
 	number_of_tests = 100
 
 	# Number of most common words
-	vector_size = 500
+	vector_size = 600
 
+	# Extracting features and labels data
 	# dictionary, mails_count = make_dictonary(data_dir, vector_size, rm_stop_words, stemming, tokenization)
 	# features, labels = extract_features_and_labels(data_dir, dictionary, mails_count)
 	
+	# Saving features and labels to files
 	# np.save(data_dir[3:] + '_' + str(vector_size) + '_MinMax_features', features)
 	# np.save(data_dir[3:] + '_' + str(vector_size) + '_MinMax_labels' , labels)
 	
+	# Reading features and labels from files
 	features =  np.load(data_dir[3:] + '_' + str(vector_size) + '_MinMax_features.npy')
 	labels = np.load(data_dir[3:] + '_' + str(vector_size) + '_MinMax_labels.npy')
 
@@ -55,19 +58,18 @@ if __name__ == '__main__':
 	# results_dir = results_dir + data_dir[3:] + '_' + str(vector_size) + '_GridSearch/'
 
 	algorithms = get_random_search_tuned_algorithms(features, labels, n_splits, n_iter, standard_scale, min_max_scale)
-	results_dir = results_dir + data_dir[3:] + '_' + str(vector_size) + '_RandomSearchNNOnly/'
+	results_dir = results_dir + data_dir[3:] + '_' + str(vector_size) + '_RandomSearch/'
 
 	algorithms = evaluate_algorithms(algorithms, features, labels, number_of_tests, n_splits, standard_scale, min_max_scale)
 
 	save_test_results(algorithms, features, labels, results_dir, time()-start)
-	create_plots(algorithms, results_dir, number_of_tests)
 
 
-	# Test for finding opimal vector size
-	# words_begin = 100
-	# words_end = 1100
-	# words_step = 100
+	# Finding opimal vector size
+	# size_begin = 100
+	# size_end = 1100
+	# size_step = 100
 	# results_dir = results_dir + data_dir[3:] + '_DurationAccurasyRatio'
-	# vector_duration_accurasy_ratio(number_of_tests, n_splits, data_dir, words_begin, words_end, words_step, standard_scale, min_max_scale, rm_stop_words, stemming, tokenization, results_dir)
+	# vector_duration_accurasy_ratio(number_of_tests, n_splits, data_dir, size_begin, size_end, size_step, standard_scale, min_max_scale, rm_stop_words, stemming, tokenization, results_dir)
 
 
